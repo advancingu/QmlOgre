d7ae2797217237a2a720eb77f76582b9fc98728d
Markus Weiland <mw@graph-ix.net>
2013-08-14 11:43:02 -0400

    Updated LICENSE information.

--- lib/ogreengineitem.h
+++ lib/ogreengineitem.h
@@ -1,3 +1,12 @@
+/*!
+ * \copyright (c) Nokia Corporation and/or its subsidiary(-ies) (qt-info@nokia.com) and/or contributors
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * \license{This source file is part of QmlOgre abd subject to the BSD license that is bundled
+ * with this source code in the file LICENSE.}
+ */
+
 #ifndef OGREENGINEITEM_H
 #define OGREENGINEITEM_H
 
