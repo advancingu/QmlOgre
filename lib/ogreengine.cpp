756f476dfabf3c358b7263d8ed9d5dbd108b670f
Markus Weiland <mw@graph-ix.net>
2013-08-14 22:02:34 -0400

    Added unbinding of additional GL buffers on doneOgreContext().
    Added glPopClientAttrib() and glPushClientAttrib() to glPopAttrib() and glPushAttrib().

--- lib/ogreengine.cpp
+++ lib/ogreengine.cpp
@@ -80,6 +80,7 @@ void OgreEngine::setQuickWindow(QQuickWindow *window)
 void OgreEngine::activateOgreContext()
 {
     glPopAttrib();
+    glPopClientAttrib();
 
     m_qtContext->functions()->glUseProgram(0);
     m_qtContext->doneCurrent();
@@ -93,10 +94,27 @@ void OgreEngine::doneOgreContext()
     m_ogreContext->functions()->glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     m_ogreContext->functions()->glBindRenderbuffer(GL_RENDERBUFFER, 0);
     m_ogreContext->functions()->glBindFramebuffer(GL_FRAMEBUFFER_EXT, 0);
+
+    // unbind all possible remaining buffers; just to be on safe side
+    m_ogreContext->functions()->glBindBuffer(GL_ARRAY_BUFFER, 0);
+    m_ogreContext->functions()->glBindBuffer(GL_ATOMIC_COUNTER_BUFFER, 0);
+    m_ogreContext->functions()->glBindBuffer(GL_COPY_READ_BUFFER, 0);
+    m_ogreContext->functions()->glBindBuffer(GL_COPY_WRITE_BUFFER, 0);
+    m_ogreContext->functions()->glBindBuffer(GL_DRAW_INDIRECT_BUFFER, 0);
+//    m_ogreContext->functions()->glBindBuffer(GL_DISPATCH_INDIRECT_BUFFER, 0);
+    m_ogreContext->functions()->glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    m_ogreContext->functions()->glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
+    m_ogreContext->functions()->glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+//    m_ogreContext->functions()->glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+    m_ogreContext->functions()->glBindBuffer(GL_TEXTURE_BUFFER, 0);
+    m_ogreContext->functions()->glBindBuffer(GL_TRANSFORM_FEEDBACK_BUFFER, 0);
+    m_ogreContext->functions()->glBindBuffer(GL_UNIFORM_BUFFER, 0);
+
     m_ogreContext->doneCurrent();
 
     m_qtContext->makeCurrent(m_quickWindow);
     glPushAttrib(GL_ALL_ATTRIB_BITS);
+    glPushClientAttrib(GL_CLIENT_ALL_ATTRIB_BITS);
 }
 
 QOpenGLContext* OgreEngine::ogreContext() const
